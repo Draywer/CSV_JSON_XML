f77000f475ffe4e9ab81fd1668d2b63d0f7a5261
Alexander Gnatenko <agnatenko@gmail.com>
2021-01-01 19:35:50 +0300

    Редактирование JSONFile.java

--- src/main/java/ru/idcore/service/CSVFile.java
+++ src/main/java/ru/idcore/service/CSVFile.java
@@ -1,16 +1,25 @@
 package ru.idcore.service;
 
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Alexander Gnatenko
  */
 public abstract class CSVFile<T> {
-    public abstract Class<T> getTType();
+    abstract Class<T> getType();
 
     public void about() {
         System.out.println("Библиотека для работы с CSV-файлами");
@@ -18,11 +27,46 @@ public abstract class CSVFile<T> {
 
     public List<T> getCSVObj(String fileName, Character separator) throws FileNotFoundException {
           return new CsvToBeanBuilder<T>(new FileReader(fileName))
-                .withType(getTType())
+                .withType(getType())
                 .withSeparator(separator)
                 .build()
                 .parse();
 
+    }
+
+    public List<T> getObjFromCSVFileWithHeader(String csvFile, Character csvParser) {
+        List<T> tList = new ArrayList<>();
+        CSVParser parser = new CSVParserBuilder().withSeparator(csvParser).build();
+
+        try (CSVReader csvReader = new CSVReaderBuilder(new FileReader(csvFile)).withCSVParser(parser).build()) {
+            HeaderColumnNameMappingStrategy<T> strategy = new HeaderColumnNameMappingStrategy<>();
+            strategy.setType(getType());
+            tList = new CsvToBeanBuilder<T>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build()
+                    .parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tList;
+    }
+
+    public List<T> getObjFromCSVFile(String csvFile, Character csvParser, String[] objColumns) {
+        List<T> tList = new ArrayList<>();
+        CSVParser parser = new CSVParserBuilder().withSeparator(csvParser).build();
+
+        try (CSVReader csvReader = new CSVReaderBuilder(new FileReader(csvFile)).withCSVParser(parser).build()) {
+            ColumnPositionMappingStrategy<T> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(getType());
+            strategy.setColumnMapping(objColumns);
+            tList = new CsvToBeanBuilder<T>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build()
+                    .parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tList;
     }
 
 }
